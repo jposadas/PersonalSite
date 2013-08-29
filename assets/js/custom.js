9669618325620b3a1c6b7a40e6142692f9b8fddd
Juan Posadas <jposadas@stanford.edu>
2013-08-29 02:27:52 -0700

    created social links

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+	$('#linkedin_icon').click(function() {
+		window.open("http://www.linkedin.com/pub/juan-posadas-castillo/6b/22a/213");
+	});
+
+	$('#facebook_icon').click(function() {
+		window.open("https://www.facebook.com/jposadas92");
+	});
+
+	$('#twitter_icon').click(function() {
+		window.open("https://twitter.com/jposadasc");
+	});
+
+	$('#github_icon').click(function() {
+		window.open("https://github.com/jposadas");
+	});
+
+	$('#email_icon').click(function () {
+		window.location.href = "mailto:jposadas@stanford.edu";
+	});
+
+	$('#peru-link').click(function () {
+		window.open("https://en.wikipedia.org/wiki/Peru");
+	});
+
+	$('#stanford-link').click(function () {
+		window.open("https://www.stanford.edu");
+	});
+
+});
